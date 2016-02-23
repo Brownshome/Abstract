d300aa77b7763eb9f25b2adaa89742f0b713864a
James Brown <jamesphone180@gmail.com>
2016-02-23 20:30:12 +1300

    Fixed UI failure on Intel drivers

--- res/shaders/ui-fragment.glsl
+++ res/shaders/ui-fragment.glsl
@@ -4,9 +4,10 @@ layout(location = 0) in vec4 in_colour;
 layout(location = 1) flat in uint in_id;
 
 layout(location = 0) out vec4 out_colour;
-layout(location = 1) out uint out_id;
+layout(location = 1) out vec4 out_id;
 
 void main() {
 	out_colour = in_colour;
-	out_id = in_id;
+	out_id.r = uintBitsToFloat(in_id);
+	out_id.a = 1;
 }
