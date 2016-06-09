6d4fd09959f40dd3313ea5813f5003155e5697b2
James Brown <jamesphone180@gmail.com>
2016-06-09 16:24:48 +1200

    Felt like orange today

--- src/resources/shaders/model-fragment.glsl
+++ src/resources/shaders/model-fragment.glsl
@@ -5,6 +5,6 @@ layout(location = 0) in vec3 in_normal;
 layout(location = 0) out vec4 out_colour;
 
 void main() {
-	out_colour.rgb = vec3(0.4, 0.4, 0.6) * (abs(dot(in_normal, normalize(vec3(0.5, 2, 1)))) * 0.5 + 0.5);
+	out_colour.rgb = vec3(1, 0.8, 0) * (abs(dot(in_normal, normalize(vec3(0.5, 2, 1)))) * 0.5 + 0.5);
 	out_colour.a = 1.0;
 }
